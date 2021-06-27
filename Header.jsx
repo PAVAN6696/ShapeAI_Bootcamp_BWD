38382b7f4c4e656160cb4a2b0828449e8106b07c
PAVAN6696 <84792484+PAVAN6696@users.noreply.github.com>
2021-06-27 13:45:26 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return (
+    <header>
+      <h1>ShapeAI Bootcamps</h1>
+      <p>created by Pavan sai nadh gupta</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div className="note">
+     <h1>Javasacript and React.js</h1>
+     <p>A basic web dev React js bootcamp</p>
+     <p>we learn the knowledge from you @ ShapeAI</p>
+    </div>
+  );
+}
+
+export default Info;
